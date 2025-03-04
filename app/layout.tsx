880236181b4464ca56b96671fffba0cee51b84ec
th-saisaketh <saisaketh.gootykase@gmail.com>
2025-03-04 01:01:18 -0500

    Update name

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"https://synquor.vercel.app/"}>Orbi{bigDeltaUnicode}ccel</Link>
+                    <Link href={"https://synquor.vercel.app/"}>Orbi{bigDeltaUnicode}ccel_</Link>
                   </div>
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
                 </div>
